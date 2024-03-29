0ef7a5938b81df1b1f0738143883787a78c22c6d
IvoSa92 <ivo_sauter@web.de>
2024-03-29 19:46:10 +0100

    added function deleteItem with other supportive functions

--- script.js
+++ script.js
@@ -49,6 +49,56 @@ class BinaryTree {
     let newInsert = new Node(value);
     this.insertNode(this.root, newInsert);
   }
+
+  deleteItem(value, root = this.root) {
+    if (root === null) return null;
+
+    if (value < root.data) {
+      root.left = this.deleteItem(value, root.left);
+    } else if (value > root.data) {
+      root.right = this.deleteItem(value, root.right);
+    } else {
+      if (root.left === null) {
+        return root.right;
+      } else if (root.right === null) {
+        return root.left;
+      }
+
+      root.data = this.findMin(root.right).data;
+      root.right = this.deleteNode(root.right, root.data);
+    }
+    return root;
+  }
+
+  findMin(node) {
+    let current = node;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current;
+  }
+
+  deleteNode(root, value) {
+    if (root === null) {
+      return root;
+    }
+    if (value < root.data) {
+      root.left = this.deleteNode(root.left, value);
+    } else if (value > root.data) {
+      root.right = this.deleteNode(root.right, value);
+    } else {
+      if (root.left === null) {
+        return root.right;
+      } else if (root.right === null) {
+        return root.left;
+      }
+
+      let minNode = this.findMin(root.right);
+      root.data = minNode.data;
+      root.right = this.deleteNode(root.right, minNode.data);
+    }
+    return root;
+  }
 }
 
 const data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
@@ -72,6 +122,7 @@ tree.buildTree(data);
 tree.insert(17);
 tree.insert(18);
 tree.insert(98);
-tree.insert(98);
-tree.insert(98);
+tree.insert(112);
+tree.deleteItem(17);
+tree.deleteItem(324);
 prettyPrint(tree.root);
